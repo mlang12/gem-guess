7542ba6a78de7ba19787a6f33f7d6d22e9d797f5
Mark A Lang <mlang12@Marks-MacBook-Pro.local>
2017-04-18 18:26:05 -0400

    initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  var red = $(".red");
+  var yellow = $(".yellow");
+  var green = $(".green");
+  var blue = $(".blue");
+  var randomNum = $(".random-number");
+  var guessSum = $(".guess-sum");
+  var gem = $(".gem");
+
+  var game = {
+    rand: 0,
+    sum: 0,
+    getRandom: function(){
+      return 
+    },
+    init: function(){
+
+    },
+    redraw: function(){
+
+    }
+  }
+
+
+})
